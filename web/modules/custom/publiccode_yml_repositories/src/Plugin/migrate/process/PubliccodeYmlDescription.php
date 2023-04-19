54cb64df20f97d2baaba56c978d2b3ee2bfcda2b
itamair <itamair@me.com>
2023-04-20 00:27:45 +0200

    publiccode_yml_repositories custom module updated

--- web/modules/custom/publiccode_yml_repositories/src/Plugin/migrate/process/PubliccodeYmlDescription.php
+++ web/modules/custom/publiccode_yml_repositories/src/Plugin/migrate/process/PubliccodeYmlDescription.php
@@ -2,9 +2,13 @@
 
 namespace Drupal\publiccode_yml_repositories\Plugin\migrate\process;
 
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\migrate\Plugin\MigrationInterface;
 use Drupal\migrate\ProcessPluginBase;
 use Drupal\migrate\MigrateExecutableInterface;
 use Drupal\migrate\Row;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\publiccode_yml_repositories\Services\PubliccodeYmlParserInterface;
 
 /**
  * Returns the Description It value of the PubliccodeYm content.
@@ -15,15 +19,50 @@ use Drupal\migrate\Row;
  *   id = "publiccode_yml_description"
  * )
  */
-class PubliccodeYmlDescription extends ProcessPluginBase {
+class PubliccodeYmlDescription extends ProcessPluginBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The Publiccode Yml Parser Service.
+   *
+   * @var \Drupal\publiccode_yml_repositories\Services\PubliccodeYmlParserInterface
+   */
+  protected $publiccodeYmlParser;
+
+  /**
+   * Constructs a ProcessField plugin.
+   *
+   * @param array $configuration
+   *   The plugin configuration.
+   * @param string $plugin_id
+   *   The plugin ID.
+   * @param mixed $plugin_definition
+   *   The plugin definition.
+   * @param \Drupal\publiccode_yml_repositories\Services\PubliccodeYmlParserInterface $publiccode_yml_parser
+   *   The field plugin manager.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, PubliccodeYmlParserInterface $publiccode_yml_parser) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->publiccodeYmlParser = $publiccode_yml_parser;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition, MigrationInterface $migration = NULL) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('publiccode_yml_parser')
+    );
+  }
 
   /**
    * {@inheritdoc}
    */
   public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
-    /** @var \Drupal\publiccode_yml_repositories\Services\PubliccodeYmlParserInterface $publiccode_yml_parser */
-    $publiccode_yml_parser = \Drupal::service('publiccode_yml_parser');
-    $description_block = $publiccode_yml_parser->descriptionBlock($value);
+    // Get the Description array block from the PubliccodeYaml content.
+    $description_block = $this->publiccodeYmlParser->descriptionBlock($value);
     $description = '';
     $langcode = $row->getSourceProperty('langcode') ?? 'it';
     if (!empty($description_block) &&
